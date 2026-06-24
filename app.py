46ea6efafe4d5008ec475df247f3689434771e2b
Vishrutaditya Joshi <vishrutaditya.joshi@gmail.com>
2026-06-24 12:37:44 +0530

    Added stock analysis UI

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("Financial Research AI Agent")
+import streamlit as st
+import yfinance as yf
+
+st.title("Indian Stock Research Assistant")
+
+stock_symbol = st.text_input(
+    "Enter NSE Stock Symbol",
+    "TCS"
+)
+
+if st.button("Analyze"):
+
+    try:
+        ticker = yf.Ticker(stock_symbol + ".NS")
+
+        info = ticker.info
+
+        st.success("Stock Data Retrieved")
+
+        st.write("Company Name:", info.get("longName"))
+        st.write("Current Price:", info.get("currentPrice"))
+        st.write("Market Cap:", info.get("marketCap"))
+        st.write("Sector:", info.get("sector"))
+
+    except Exception as e:
+        st.error(str(e))
--- services/stock_data.py
+++ services/stock_data.py
@@ -1,1 +1,9 @@
 print("Stock Data Service Loaded")
+import yfinance as yf
+
+ticker = yf.Ticker("TCS.NS")
+
+info = ticker.info
+
+print("Company:", info.get("longName"))
+print("Current Price:", info.get("currentPrice"))
