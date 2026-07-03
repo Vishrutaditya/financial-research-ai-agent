a844f7bc3156730d3d7bf0f362ab3ec61cefa63f
Vishrutaditya Joshi <vishrutaditya.joshi@gmail.com>
2026-07-03 17:58:13 +0530

    structure change and created prompt files

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import streamlit as st
-import yfinance as yf
+from services.stock_data import get_stock_info, get_historical_data, get_company_info
 
 st.title("Indian Stock Research Assistant")
 
 stock_symbol = st.text_input(
     "Enter NSE Stock Symbol",
-    "TCS"
+    ""
 )
 
 if st.button("Analyze"):
+    if not stock_symbol:
+        st.error("please enter a stock symbol.")
+    else:
+        try:
+            company = get_company_info(stock_symbol.upper() + ".NS")
 
-    try:
-        ticker = yf.Ticker(stock_symbol + ".NS")
+            st.write("Company Name:", company["name"])
+            st.write("Sector:", company["sector"])
+            st.write("Industry:", company["industry"])
+            st.write("Market Cap:", company["market_cap"])
+            st.write("Website:", company["website"])
 
-        info = ticker.info
-
-        st.success("Stock Data Retrieved")
-
-        st.write("Company Name:", info.get("longName"))
-        st.write("Current Price:", info.get("currentPrice"))
-        st.write("Market Cap:", info.get("marketCap"))
-        st.write("Sector:", info.get("sector"))
-
-    except Exception as e:
-        st.error(str(e))
+        except Exception as e:
+            st.error(str(e))
+    
